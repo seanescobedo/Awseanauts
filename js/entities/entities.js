b80adf23f295b172cf55d921efd41852544d2cee
sean escobedo <seancandoit68@gmail.com>
2015-03-10 09:18:37 -0700

    k

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -48,3 +48,77 @@ init: function(x, y, settings) {
       return true;
     }
 });
+
+game.PlayerBaseEntity = me.Entity.extend({
+init: function(x, y, settings) {
+    this._super(me.Entity, 'init', [x, y, {
+            image: "tower",
+            spritewidth: "100",
+            spriteheight: "100",
+            width: 100,
+            height: 100,
+            getShape: function() {
+                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+            }
+        }]);
+         this.broken = false;
+         this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "PlayerBaseEntity";
+        
+    },
+    
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+        
+});
+
+game.EnemyBaseEntity = me.Entity.extend({
+init: function(x, y, settings) {
+    this._super(me.Entity, 'init', [x, y, {
+            image: "tower",
+            spritewidth: "100",
+            spriteheight: "100",
+            width: 100,
+            height: 100,
+            getShape: function() {
+                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+            }
+        }]);
+         this.broken = false;
+         this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "EnemyBaseEntity";
+        
+    },
+    
+    update:function(delta){
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+        
+});
